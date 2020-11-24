c9b0647676804ac438eb94a7af03602bb0444bbc
Obin Sturm <54367380+obin1@users.noreply.github.com>
2020-11-23 16:08:39 -0800

    Removed MOSAIC_BOX
    make repository public

--- MOSAIC_BOX/clean.sh
+++
@@ -1,9 +0,0 @@
-# clean.sh
-set verbose
-
-cd  compile
-make  clean
-
-cd  ..
-
-unset verbose
--- MOSAIC_BOX/experiments/aer.sh
+++
@@ -1,8 +0,0 @@
-# compile.sh
-set verbose
-
-/msrc/lapps/pgi-7.1/linux86-64/7.1/bin/pgf90 -o aer_input.x -g -C -Mextend=132 aer_input.f90
-
-aer_input.x
-
-unset verbose
